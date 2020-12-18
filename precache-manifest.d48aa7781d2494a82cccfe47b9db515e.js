9c4e3b3b80edcacfcb8faba510db1216b44d5be5
nikldev0 <57417252+nikldev0@users.noreply.github.com>
2020-12-18 22:40:27 +0000

    Updates

---
+++ precache-manifest.d48aa7781d2494a82cccfe47b9db515e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e443fe656e56ac98b4a0e8befbd7d86f",
+    "url": "/2020booklist/index.html"
+  },
+  {
+    "revision": "c65f6b757d2f47dd8489",
+    "url": "/2020booklist/static/css/main.f2e9d612.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/2020booklist/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020booklist/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65f6b757d2f47dd8489",
+    "url": "/2020booklist/static/js/main.d3f5d932.chunk.js"
+  },
+  {
+    "revision": "151f0138211d3bc5523d",
+    "url": "/2020booklist/static/js/runtime-main.b171cd99.js"
+  },
+  {
+    "revision": "665ef5c3cf89db3f966f03f26f0b396f",
+    "url": "/2020booklist/static/media/BagnardSans.665ef5c3.otf"
+  }
+]);
